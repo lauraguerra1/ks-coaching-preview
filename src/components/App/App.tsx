7af523fc49e6ecdb5fd2fede90ce0fbf1033e334
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-15 12:37:00 -0400

    Add Browser Router and starting assets

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import NavBar from '../NavBar/NavBar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
