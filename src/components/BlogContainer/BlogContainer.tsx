511a4b0b00840e44ae49c66f558c2f869e871916
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-16 17:23:51 -0400

    Add featued overlays

--- src/components/BlogContainer/BlogContainer.tsx
+++ src/components/BlogContainer/BlogContainer.tsx
@@ -6,10 +6,18 @@ const BlogContainer = () => {
     <>
       <section className='blog-landing'>
         <img className='blog-landing-img' src={KSPortrait} alt='portrait of katie sanger looking into the camera with green foliage in the background' />
-        <article>
-          <h1>BLOG</h1>
-          <p>Browse creative, meaningful & insightful musings on motherhood, well being, addiction + recovery, mental health, spirituality, psychology and astrology</p>
-        </article>
+        <div className='article-container'>
+          <article style={{alignSelf: 'flex-start'}}>
+            <h1>BLOG</h1>
+            <p>Browse creative, meaningful & insightful musings on motherhood, well being, addiction + recovery, mental health, spirituality, psychology and astrology</p>
+          </article>
+          <article id='featuredBlog'>
+            <p>Read Our Latest:</p>
+            <h2>Do Nothing.</h2>
+            <p>The power of doing nothing in times of uncertainty.</p>
+            <button className='view-post-btn'>view the post</button>
+          </article>
+        </div>
       </section>
     </>
   )
