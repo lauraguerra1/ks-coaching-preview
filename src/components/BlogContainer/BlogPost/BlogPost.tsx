5dfc42f90c2a66fab48e1ad98271a36cf70620a9
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-16 21:24:16 -0400

    Add animation to blog landing articles

--- src/components/BlogContainer/BlogPost/BlogPost.tsx
+++ src/components/BlogContainer/BlogPost/BlogPost.tsx
@@ -8,14 +8,14 @@ type BlogProps = {
 
 const BlogPost = ({ blog, isLast }: BlogProps) => {
   return (
-    <section className={isLast ? 'last-blog' : 'blog'}>
+    <a href={blog.link} target='-blank'className={isLast ? 'last-blog' : 'blog'}>
       <article className='blog-article'>
         <p className='blog-p'><i style={{fontFamily: "'Playfair Display', serif"}}>{blog.tag}</i></p>
         <h3 className='blog-title' style={{fontFamily: "'Playfair Display', serif", fontWeight: '400'}}>{blog.title}</h3>
         <p className='blog-p'>{blog.tagline}</p>
       </article>
       <img className='blog-img' src={blog.coverImg} alt={blog.title} />
-    </section>
+    </a>
   )
 }
 
