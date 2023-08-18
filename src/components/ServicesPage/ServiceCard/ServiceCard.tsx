6fbee5f158f7d258aca188516de4412028f3c87e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-18 18:32:59 -0400

    Add styling for service cards

--- src/components/ServicesPage/ServiceCard/ServiceCard.tsx
+++ src/components/ServicesPage/ServiceCard/ServiceCard.tsx
@@ -4,12 +4,13 @@ import { Service } from '../../../types'
 const ServiceCard = ({ service, number}: { service: Service, number: number }) => {
   const scrollToService = (serviceNumber: number) => {
     document.querySelector(`.service${serviceNumber}`)?.scrollIntoView()
+    console.log(document.querySelector(`.service${serviceNumber}`))
   }
   return (
     <section className='service-card'>
-      <img className='service-card-image' src={service.image.src} alt={service.image.alt} />
-      <div className='service-card-overlay'>
-        <button className='service-card-button' onClick={() => scrollToService(number)}>Learn More</button>
+        <img className='service-card-image' src={service.image.src} alt={service.image.alt} />
+      <div className='service-card-overlay' onClick={() => scrollToService(number)}>
+        <button className='service-card-button' >Learn More</button>
       </div>
       <h2 className='service-card-title'>{service.title}</h2>
     </section>
--- src/components/ServicesPage/ServicesPage.tsx
+++ src/components/ServicesPage/ServicesPage.tsx
@@ -5,8 +5,16 @@ import ServiceCard from './ServiceCard/ServiceCard'
 const ServicesPage = () => {
   const serviceEls = services.map((service, i) => <ServiceCard key={service.title} service={service} number={i+1} />)
   return (
-    <section className='service-cards-container'>
-      {serviceEls}
+    <section className='service-page'>
+      <header>
+        <h1 style={{fontSize: '40px'}}>Services</h1>
+      </header>
+      <section className='service-cards-container'>
+        {serviceEls}
+      </section>
+      <section>
+          <p className='service1'>service 1</p>
+      </section>
     </section>
   )
 }
