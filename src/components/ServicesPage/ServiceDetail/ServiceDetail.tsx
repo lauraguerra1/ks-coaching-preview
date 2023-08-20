ed6cc59286d405ce46eeb8daa5cd3d3ae9361020
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-20 17:17:07 -0400

    Add scroll fade animation

--- src/components/ServicesPage/ServiceDetail/ServiceDetail.tsx
+++ src/components/ServicesPage/ServiceDetail/ServiceDetail.tsx
@@ -1,6 +1,7 @@
 import './ServiceDetail.css'
 import { Service } from '../../../types'
 import back from '../../../assets/back.png'
+import {useState, useRef, useEffect} from 'react'
 
 type ServiceDetailProps = {
   service: Service,
@@ -10,15 +11,26 @@ type ServiceDetailProps = {
 }
 
 const ServiceDetail = ({ service, number, serviceIsSelected, deselectService }: ServiceDetailProps) => {
+  const [isVisible, setVisible] = useState(true);
+  const domRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current!);
+    return () => observer.unobserve(domRef.current!);
+  }, []);
+
   return (
-    <section style={{width: '100%'}} className='small-screen-sevice-container'>
+    <section style={{ width: '100%' }} ref={domRef}>
       {serviceIsSelected &&
         <div className='selected-service-top'>
           <button className='service-back-btn' onClick={deselectService}><img src={back} alt='back button' /></button>
           <h2 className='service-detail-title'>{service.title}</h2>
         </div>
       }
-      <section className='service-detail' id={`service${number}`}>
+      <section className={`service-detail ${isVisible? 'visible' : ''}`} id={`service${number}`}>
         <div className='service-detail-image-container'>
           <img className='service-detail-image' src={service.image.src} alt={service.image.alt} />
           <button className='book-service-button'>BOOK NOW</button>
